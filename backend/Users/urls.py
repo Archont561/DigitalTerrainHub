16f35e80e8edab0d8c4611a5a344cdab27d92378
Eryk Srebrny <esrebrny@onet.eu>
2025-04-25 18:25:06 +0200

    refactor: rearange Users app urls

--- backend/Users/urls.py
+++ backend/Users/urls.py
@@ -1,18 +1,20 @@
-from django.urls import path
-from .views import ( 
-    UserRegisterView,
-    UserLoginView,
-    UserLogoutView,
-    UserUpdateView,
-    UserDeleteView,
-    UserProfileUpdateView
-)
+from django.urls import path, include
+from . import views
+
+profile_patterns = [
+    path('', views.UserProfileView.as_view(), name='home'),
+    path('update/', views.UserProfileUpdateView.as_view(), name='update'),
+]
+
+user_patterns = [
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('update/', views.UserUpdateView.as_view(), name='update'),
+    path('delete/', views.UserDeleteView.as_view(), name='delete'),
+]
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='user-register'),
-    path('login/', UserLoginView.as_view(), name='user-login'),
-    path('logout/', UserLogoutView.as_view(), name='user-logout'),
-    path('update/', UserUpdateView.as_view(), name='user-update'),
-    path('delete/', UserDeleteView.as_view(), name='user-delete'),
-    path('profile/update/', UserProfileUpdateView.as_view(), name='user-profile-update'),
+    path("", include((user_patterns, "user"))),
+    path("profile/", include((profile_patterns, "profile"))),
 ]
