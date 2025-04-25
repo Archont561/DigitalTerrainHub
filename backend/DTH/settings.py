eaf912f1844685987d5f39922903775350ff5725
Eryk Srebrny <esrebrny@onet.eu>
2025-04-25 13:42:25 +0200

    feat: add django-htmx extension

--- backend/DTH/settings.py
+++ backend/DTH/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_vite',
+    'django_vite',    
+    'django_htmx',
     ######,
     "Users",
     "Payment",
@@ -35,6 +36,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ####
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'DTH.urls'
