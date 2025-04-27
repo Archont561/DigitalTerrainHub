27118579c58c54f5a705a910eb8fc78ac10975dd
Eryk Srebrny <esrebrny@onet.eu>
2025-04-27 21:33:46 +0200

    refactor: implement ContextMixin to handle common context across views

--- backend/DTH/views.py
+++ backend/DTH/views.py
@@ -2,53 +2,47 @@ from django.conf import settings
 from django.views.generic import TemplateView
     
 
-class HomeView(TemplateView):
+class ContextMixin:
+    context_extra = {}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(self.context_extra)
+        return context
+
+
+class HomeView(ContextMixin, TemplateView):
     template_name = "pages/classic/home.html"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "home": True, 
-            'app_name': settings.APP_NAME,
-        })
-        return context
+    context_extra = {
+        "home": True, 
+        "app_name": settings.APP_NAME,
+    }
 
 
-class AboutView(TemplateView):
+class AboutView(ContextMixin, TemplateView):
     template_name = "pages/classic/about.html"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "about": True,
-            'app_name': settings.APP_NAME,
-        })
-        return context
+    context_extra = {
+        "about": True, 
+        "app_name": settings.APP_NAME,
+    }
 
 
-class ContactView(TemplateView):
-    template_name = "pages/classic/contact.html"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "contact": True,
-            'app_name': settings.APP_NAME,
-        })
-        return context
+class ContactView(ContextMixin, TemplateView):
+    template_name = "pages/classic/contact.html"    
+    context_extra = {
+        "contact": True, 
+        "app_name": settings.APP_NAME,
+    }
 
 
-class Custom404View(TemplateView):
+class Custom404View(ContextMixin, TemplateView):
     template_name = "pages/classic/404.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'app_name': settings.APP_NAME,
-        })
-        return context
+    context_extra = {
+        "app_name": settings.APP_NAME,
+    }
 
     def render_to_response(self, context, **kwargs):
         response = super().render_to_response(context, **kwargs)
         response.status_code = 404
         return response
+
