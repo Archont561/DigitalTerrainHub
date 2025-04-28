d9946dc53def2eeba098e5bb0c0d5d0f69885891
Eryk Srebrny <esrebrny@onet.eu>
2025-04-29 01:33:39 +0200

    feat: add humanize to INSTALLED_APPS

--- backend/DTH/settings.py
+++ backend/DTH/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     # django extensions
     'django_vite',    
     'django_htmx',
