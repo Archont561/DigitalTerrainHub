b595373b7485e4a34d9f14a26e83a699d1b6d56d
Eryk Srebrny <esrebrny@onet.eu>
2025-05-05 13:01:34 +0200

    feat: add django-tailwind extension

--- backend/DTH/urls.py
+++ backend/DTH/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('pyodm/', include('PyODM.urls')),
     path('map/', include('MapViewer.urls')),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 handler404 = views.Custom404View.as_view()
