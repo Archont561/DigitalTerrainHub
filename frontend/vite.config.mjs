9954b1a85ce7e7607b13d962b433884ed60e7332
Eryk Srebrny <esrebrny@onet.eu>
2025-05-05 02:40:50 +0200

    refactor: adjust path to entry file

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,25 +1,31 @@
 import { defineConfig } from 'vite';
-import { resolve } from "path";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite"
 
-export default defineConfig({
-    base: "/static/",
-    resolve: {
-        alias: {
-            "@": resolve("./static")
-        }
-    },
-    build: {
-        manifest: "manifest.json",
-        outDir: resolve("./assets"),
-        assetsDir: "django-assets",
-        rollupOptions: {
-            input: {
-                test: resolve('./static/js/main.js')
-            },
-        }
-    },
-    plugins: [
-        tailwindcss()
-    ]
+export default defineConfig(() => {
+    const staticDir = path.resolve("static");
+    const jsDir = path.join(staticDir, "js");
+
+    return {
+        base: "/static/",
+        resolve: {
+            alias: {
+                "@": path.resolve("./static")
+            }
+        },
+        build: {
+            manifest: "manifest.json",
+            outDir: path.resolve("dist"),
+            emptyOutDir: true,
+            assetsDir: "django-assets",
+            rollupOptions: {
+                input: {
+                    main: path.join(jsDir, "main.js"),
+                }
+            }
+        },
+        plugins: [
+            tailwindcss()
+        ]
+    }
 })
