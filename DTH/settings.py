d0e8c9bde656cd30c919c916144ffb5825d99098
Eryk Srebrny <esrebrny@onet.eu>
2025-05-07 22:48:15 +0200

    update: change login redirect url to profile page

--- DTH/settings.py
+++ DTH/settings.py
@@ -99,7 +99,7 @@ USE_I18N = True
 USE_TZ = True
 
 LOGIN_URL = "/users/login/"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/users/profile/"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -107,6 +107,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TAILWIND_APP_NAME = 'theme'
+TAILWIND_CSS_PATH = 'dist/styles.css'
 NPM_BIN_PATH = shutil.which("npm")
 
 DJANGO_VITE = {
