85b5cda5f88700aecda61287d6cc0ee331041e4c
Eryk Srebrny <esrebrny@onet.eu>
2025-05-13 14:29:08 +0200

    feat: add basic email configuration

--- app/Config/django/base.py
+++ app/Config/django/base.py
@@ -11,6 +11,13 @@ APP_NAME = "Digital Terrain Hub"
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
 SECRET_KEY = env("DJANGO_SECRET_KEY", default="DUMMY_SECRET_KEY")
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
+EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+DEFAULT_FROM_EMAIL = env("DJANGO_APP_EMAIL", 
+    default=f'{"".join([chunk.lower() for chunk in APP_NAME.split(" ")])}@localhost')
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
--- app/Config/django/production.py
+++ app/Config/django/production.py
@@ -3,3 +3,7 @@ from Config.env import env
 
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
