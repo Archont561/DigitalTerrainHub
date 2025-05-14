6bc7b8224625910a04c357d8e0f8a6969712cf95
Eryk Srebrny <esrebrny@onet.eu>
2025-05-14 15:37:03 +0200

    feat: change spaceless middleware implementation

--- app/Core/middleware.py
+++ app/Core/middleware.py
@@ -1,4 +1,4 @@
-from django.utils.html import strip_spaces_between_tags
+import re
 
 class SpacelessMiddleware:
     def __init__(self, get_response):
@@ -9,7 +9,11 @@ class SpacelessMiddleware:
 
         if 'text/html' in response.get('Content-Type', ''):
             content = response.content.decode('utf-8')
-            stripped_content = strip_spaces_between_tags(content)
+            stripped_content = self.strip(content)
             response.content = stripped_content.encode('utf-8')
-
         return response
+
+    def strip(self, content):
+        content = re.sub(r'>\s*<', '><', content)
+        content = re.sub(r'\s+', ' ', content)
+        return content.strip()
