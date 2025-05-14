217dae5e70743ede93b18be34cd6c7bc9b40b9c0
Eryk Srebrny <esrebrny@onet.eu>
2025-05-14 23:38:19 +0200

    chore: deleted unused Alpine component

--- app/Frontend/static_src/src/js/Alpine/loaders/AlpineComponentLoaders.js
+++ app/Frontend/static_src/src/js/Alpine/loaders/AlpineComponentLoaders.js
@@ -2,5 +2,4 @@ export default {
     optionsForm: async () => (await import('../components/optionsForm.js')).default,
     profilePage: async () => (await import('../components/profilePage.js')).default,
     uppyWidget: async () => (await import('../components/uppyWidget.js')).default,
-    workspaceList: async () => (await import('../components/workspaceList.js')).default,
 };
