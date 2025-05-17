30e31f6619c7ac65b6b61dc5ec05e84a3d0ad19a
Eryk Srebrny <esrebrny@onet.eu>
2025-05-17 17:50:33 +0200

    refactor: update imports paths

--- app/Frontend/static_src/src/ts/Alpine/loaders/AlpineComponentLoaders.ts
+++ app/Frontend/static_src/src/ts/Alpine/loaders/AlpineComponentLoaders.ts
@@ -1,9 +1,10 @@
 type AlpineComponentLoader = () => Promise<(...args: any[]) => any>;
 
 const AlpineComponentLoaders: Record<string, AlpineComponentLoader> = {
-    optionsForm: async () => (await import('../components/optionsForm')).default,
-    profilePage: async () => (await import('../components/profilePage')).default,
-    uppyWidget: async () => (await import('../components/uppyWidget')).default,
+    OptionsForm: async () => (await import('../components/OptionsForm')).default,
+    ProfilePage: async () => (await import('../components/ProfilePage')).default,
+    UppyWidget: async () => (await import('../components/UppyWidget')).default,
+    GCPMap: async () => (await import('../components/GCPMap')).default,
 };
 
 export default AlpineComponentLoaders;  
