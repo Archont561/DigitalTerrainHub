78803587fa13622cc9bbdffb6beaf06d4031bbca
Eryk Srebrny <esrebrny@onet.eu>
2025-05-19 13:04:13 +0200

    feat: create leaflet map alpinejs component

--- app/Frontend/static_src/src/ts/Alpine/components/GCPMap.ts
+++ app/Frontend/static_src/src/ts/Alpine/components/GCPMap.ts
@@ -1,24 +1,50 @@
 import * as L from "leaflet";
+import 'leaflet/dist/leaflet.css';
 
 interface GCPMapElement extends HTMLElement {
     map: ReturnType<typeof L.map>;
 }
 
+interface BindedGCP {
+    id: string;
+    image: {
+        name: string;
+        x: Number;
+        y: Number;
+    };
+    gcp: {
+        x: Number;
+        y: Number;
+        z: Number;
+    }
+}
+
 interface GCPMapComponent {
     $el: GCPMapElement;
     init(): void;
+    bindedGCPs: {
+        proj: string;
+        binding: BindedGCP[];
+    };
+    loadGCPs(): void;
+    bindGCP(image: string, GCPMarker: HTMLElement): void;
+    handleGCPFileLoading(file: string): void;
 }
 
-const gcpMap = () => {
+const GCPMap = () => {
     window.L = L;
 
     return {
         init() {
-            const map = L.map(this.$el);
+            const mapView = window.htmx.find(this.$el, ".gcp-leaflet-map");
+            const map = L.map(mapView).setView([0, 0], 2);
             this.$el.map = map;
-            
-        }
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                maxZoom: 19
+            }).addTo(map);
+        },
     } as GCPMapComponent;
 };
 
-export default gcpMap;
+export default GCPMap;
