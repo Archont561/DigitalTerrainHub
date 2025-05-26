58481eab4537b31c1ab2ca2b8c4e1582f572da41
Eryk Srebrny <esrebrny@onet.eu>
2025-05-26 18:36:42 +0200

    feat: add global presets

--- app/Config/settings/NodeODM.py
+++ app/Config/settings/NodeODM.py
@@ -3,7 +3,62 @@ from Config.env import env
 NODEODM_URL = env("NODEODM_URL", default="http://localhost:3000")
 
 GLOBAL_OPTION_PRESETS = {
-    "dummy": {
-        "option_dummy_1": "dummy_1"
-    } 
+    "Multispectral": {
+        "auto-boundary": True,
+        "radiometric-calibration": "camera"
+    },
+    "Volume Analysis": {
+        "auto-boundary": True,
+        "dsm": True,
+        "dem-resolution": "2",
+        "pc-quality": "high"
+    },
+    "3D Model": {
+        "auto-boundary": True,
+        "mesh-octree-depth": "12",
+        "use-3dmesh": True,
+        "pc-quality": "high",
+        "mesh-size": "300000"
+    },
+    "Buildings": {
+        "auto-boundary": True,
+        "mesh-size": "300000",
+        "feature-quality": "high",
+        "pc-quality": "high"
+    },
+    "Forest": {
+        "auto-boundary": True,
+        "min-num-features": "18000",
+        "use-3dmesh": True,
+        "feature-quality": "medium"
+    },
+    "DSM + DTM": {
+        "auto-boundary": True,
+        "dsm": True,
+        "dtm": True
+    },
+    "Field": {
+        "sfm-algorithm": "planar",
+        "fast-orthophoto": True,
+        "matcher-neighbors": 4
+    },
+    "Fast Orthophoto": {
+        "auto-boundary": True,
+        "fast-orthophoto": True
+    },
+    "High Resolution": {
+        "auto-boundary": True,
+        "dsm": True,
+        "pc-quality": "high",
+        "dem-resolution": "2.0",
+        "orthophoto-resolution": "2.0"
+    },
+    'Point of Interest': {
+        'mesh-size' :'300000',
+        'use-3dmesh': True
+    },
+    "Default": {
+        "auto-boundary": True,
+        "dsm": True
+    }
 }
