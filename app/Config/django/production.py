e7dd2f1fbb88f4af5b8a94d0584dba0307800bd6
Eryk Srebrny <esrebrny@onet.eu>
2025-06-01 14:51:35 +0200

    fix: add prefix to ALLOWED_HOSTS env variable

--- app/Config/django/production.py
+++ app/Config/django/production.py
@@ -2,7 +2,7 @@ from .base import *
 from Config.env import env
 
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
 
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
