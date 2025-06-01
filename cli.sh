9ca0906f75e188ef68dc614bcc563b5d73e21cec
Eryk Srebrny <esrebrny@onet.eu>
2025-06-01 16:34:38 +0200

    feat: load env variable when starting django server

--- cli.sh
+++ cli.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 set -e
+
+
+load_env() {
+  local env_file=${1:-.env}
+  if [[ -f "$env_file" ]]; then
+    echo "Loading environment variables from $env_file"
+    set -a
+    source "$env_file"
+    set +a
+  else
+    echo "Warning: Env file '$env_file' not found, skipping."
+  fi
+}
+
 MANAGE_PY="./app/manage.py"
 NPM_DIR="./app/Frontend/static_src"
+ENV_FILE="./app/.env"
 
 # Main command handler
 case "$1" in
   dev)
+    load_env $ENV_FILE
     uv run $MANAGE_PY tailwind start & uv run $MANAGE_PY runserver
     ;;
   build)
@@ -14,6 +30,7 @@ case "$1" in
     uv run $MANAGE_PY tailwind build
     ;;
   start)
+    load_env $ENV_FILE
     uv run $MANAGE_PY runserver
     ;;
   manage)
