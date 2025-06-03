79066521a67c0772074ffe11111262f2635e3d82
Eryk Srebrny <esrebrny@onet.eu>
2025-06-03 20:13:10 +0200

    refactor: move notification managing to separate model manager

--- app/Core/models.py
+++ app/Core/models.py
@@ -7,6 +7,56 @@ import uuid
 
 User = get_user_model()
 
+
+class NotificationManager(models.Manager):
+    def add(self, user, message, status=messages.INFO, extra_tags='', related_object=None, request=None):
+        content_type = None
+        related_object_id = None
+        related_object_uuid = None
+
+        if related_object:
+            content_type = ContentType.objects.get_for_model(related_object)
+            if isinstance(related_object.pk, uuid.UUID):
+                related_object_uuid = related_object.pk
+            else:
+                related_object_id = related_object.pk
+
+        notification = self.create(
+            user=user,
+            message=message,
+            status=status,
+            content_type=content_type,
+            related_object_id=related_object_id,
+            related_object_uuid=related_object_uuid,
+        )
+
+        if request is not None:
+            extra_tags = f"{extra_tags} notification_pk:{notification.pk}".strip()
+            messages.add_message(request, status, message, extra_tags=extra_tags)
+
+        return notification
+
+    def filter_by_object(self, related_object=None, related_object_uuid=None, user=None):
+        queryset = self.get_queryset()
+
+        if user:
+            queryset = queryset.filter(user=user)
+
+        if related_object:
+            content_type = ContentType.objects.get_for_model(related_object)
+            if isinstance(related_object.pk, uuid.UUID):
+                queryset = queryset.filter(related_object_uuid=related_object.pk)
+            else:
+                queryset = queryset.filter(
+                    content_type=content_type,
+                    related_object_id=related_object.pk
+                )
+        elif related_object_uuid:
+            queryset = queryset.filter(related_object_uuid=related_object_uuid)
+
+        return queryset
+
+
 class Notification(models.Model):
     STATUS_CHOICES = [
         (messages.DEBUG, 'Debug'),
@@ -34,6 +84,8 @@ class Notification(models.Model):
     related_object = GenericForeignKey('content_type', 'related_object_id')
     related_object_uuid = models.UUIDField(null=True, blank=True)
 
+    objects = NotificationManager()
+
     class Meta:
         verbose_name = "Notification"
         verbose_name_plural = "Notifications"
@@ -45,49 +97,3 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"{self.status}: {self.message[:50]}"
-
-    @classmethod
-    def add(cls, user, message, status=messages.INFO, extra_tags='', related_object=None):
-        content_type = None
-        related_object_id = None
-        related_object_uuid = None
-
-        if related_object:
-            content_type = ContentType.objects.get_for_model(related_object)
-            if isinstance(related_object.pk, uuid.UUID):
-                related_object_uuid = related_object.pk
-            else:
-                related_object_id = related_object.pk
-
-        notification = cls.objects.create(
-            user=user,
-            message=message,
-            status=status,
-            content_type=content_type,
-            related_object_id=related_object_id,
-            related_object_uuid=related_object_uuid,
-        )
-        extra_tags = f"{extra_tags} notification_pk:{notification.pk}".strip()
-        messages.add_message(request, status, message, extra_tags=extra_tags)
-        return notification
-
-    @classmethod
-    def filter_by_object(cls, related_object=None, related_object_uuid=None, user=None):
-        queryset = cls.objects.all()
-
-        if user:
-            queryset = queryset.filter(user=user)
-
-        if related_object:
-            content_type = ContentType.objects.get_for_model(related_object)
-            if isinstance(related_object.pk, uuid.UUID):
-                queryset = queryset.filter(instance_uuid=related_object.pk)
-            else:
-                queryset = queryset.filter(
-                    content_type=content_type,
-                    object_id=related_object.pk
-                )
-        elif related_object_uuid:
-            queryset = queryset.filter(related_object_uuid=related_object_uuid)
-
-        return queryset
