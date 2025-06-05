543d266b087801861001c57d5d63dbf4c0eb4907
Eryk Srebrny <esrebrny@onet.eu>
2025-06-05 02:26:55 +0200

    feat: create AstroHTMLRenderer to render html from Astro server via drf api

---
+++ app/Core/utils/drf.py
@@ -0,0 +1,13 @@
+from rest_framework.renderers import BaseRenderer
+from .http import AstroTemplateResponse
+
+class AstroHTMLRenderer(BaseRenderer):
+    media_type = "text/html"
+    format = "html"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        request = renderer_context.get('request') if renderer_context else None
+        view = renderer_context.get('view') if renderer_context else None
+        template = getattr(view, "template_name", None)
+        astroResponse = AstroTemplateResponse(request, template, data)
+        return astroResponse.content
