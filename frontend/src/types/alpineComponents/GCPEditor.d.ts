348b53b293d32ea751bc377fd9e83d0d8dfef86c
Eryk Srebrny <esrebrny@onet.eu>
2025-06-08 00:24:23 +0200

    refactor: make GCPEditor use GCPpointsDB for handling data from GCPoint model api

--- frontend/src/types/alpineComponents/GCPEditor.d.ts
+++ frontend/src/types/alpineComponents/GCPEditor.d.ts
@@ -1,32 +1,26 @@
 import type { Map as OlMap, Feature, Overlay } from 'ol';
+import type { SelectEvent } from "ol/interaction/Select";
 import type { Vector as VectorLayer, Image as ImageLayer } from 'ol/layer';
 import type { Vector as VectorSource, ImageStatic } from 'ol/source';
 import type { Point } from 'ol/geom';
 import type { Extent } from "ol/extent";
 import type { Coordinate } from 'ol/coordinate';
 import type { AlpineComponent } from 'alpinejs';
+import type { GCPointBinding } from '../../lib/db/GCPPointsDB';
 
-export type Binding = Map<UUID, BindedGCP>;
-export type Store = Map<UUID, Binding>;
+
 export type OlPoint = Feature<Point>;
 
-export interface BindedGCP {
-    gcp?: OlPoint;
-    image?: OlPoint;
-}
-
 export interface GCPEditor {
-    workspaceBinding: null | Binding;
     currentGCPFeature: null | OlPoint;
     currentImageMarkerFeature: null | OlPoint;
 
+    onImagePointSelection(event: SelectEvent): void;
+    onGCPointShiftSelection(event: SelectEvent): void;
     changeImage(imageUrl: string): void;
     registerImageMarker(marker: OlPoint, imageUrl: string): void;
     registerGCPMarker(marker: OlPoint): void;
-    bindCurrentSelection(): void ;
     loadGCPsFromFile(file: File): void;
-    getOrCreateBinding(workspaceUUID: UUID): Binding;
-    getOrSetUUID(feature: OlPoint): UUID;
     onReveal(): void;
 }
 
