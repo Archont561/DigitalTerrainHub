470acffe45815380b789216898916d1efe366e5c
Eryk Srebrny <esrebrny@onet.eu>
2025-06-08 22:48:05 +0200

    feat: create components for Notification model rendering

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -5,12 +5,14 @@ import * as Tabs from "./tabs";
 import * as Task from "./task";
 import * as Workspace from "./workspace";
 import * as Utils from "./utils";
+import * as Notifications from "./notifications";
 
 export {
     Forms,
     Ol,
     Preset,
     Tabs,
+    Notifications,
     Task,
     Workspace,
     Utils,
---
+++ frontend/src/components/notifications/index.ts
@@ -0,0 +1,11 @@
+import List from "./List.astro"
+import Partial from "./Partial.astro"
+import SSEPartial from "./SSEPartial.astro"
+import Toast from "./Toast.astro"
+
+export {
+    List,
+    Partial,
+    SSEPartial,    
+    Toast,
+}
