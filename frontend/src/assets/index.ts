0efc3abf7759af05fb1d268bb792032099afdd57
Eryk Srebrny <esrebrny@onet.eu>
2025-06-09 21:25:23 +0200

    feat: create exports for @assets

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,23 @@
+import Anon from "./anonymus.svg?url";
+import Astro from "./astro.svg?url";
+import AvailablePrests from "./dummy/availablePresets.json?url";
+import GCPoints from "./dummy/gcppoints.json?url";
+import Notification from "./dummy/notification.json?url";
+import Task from "./dummy/task.json?url";
+import User from "./dummy/user.json?url";
+import Workspace from "./dummy/workspace.json?url";
+
+
+export const SVGURLs = {
+    Anon,
+    Astro,
+};
+
+export const DummyDataURLs = {
+    AvailablePrests,
+    GCPoints,
+    Notification,
+    Task,
+    User,
+    Workspace,
+};
