476299ae26e90dde480882e014a1cca1f65b94c8
Eryk Srebrny <esrebrny@onet.eu>
2025-06-09 15:33:56 +0200

    feat: register @alpinejs/persist plugin

--- frontend/src/lib/Alpine/index.ts
+++ frontend/src/lib/Alpine/index.ts
@@ -1,7 +1,9 @@
 import type { Alpine } from 'alpinejs'
 import AlpineManager from './AlpineManager'
+import persist from "@alpinejs/persist";
 
 export default (Alpine: Alpine) => {
+    Alpine.plugin(persist);
     window.Alpine = {
         ...Alpine,
         Manager: new AlpineManager(Alpine),
