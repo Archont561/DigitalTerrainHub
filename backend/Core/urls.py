ffffd78ca8c9e1eaf2f604148e5dd3f86b5d41a7
Eryk Srebrny <esrebrny@onet.eu>
2025-06-09 19:10:03 +0200

    feat: add debugpatterns if in settings.DEBUG is true

--- backend/Core/urls.py
+++ backend/Core/urls.py
@@ -7,26 +7,30 @@ from django_eventstream.views import events
 from .api import apipatterns
 
 
-urlpatterns = [
-    path("", include([
-        path("", HomeView.as_view(), name="home"),
-    ], namespace="core")),
-    path('admin/', admin.site.urls),
-    path('events/<channel>/', events, name='events'),
-    path('api/', include(apipatterns, namespace="api"))
-]
-
-handler404 = Custom404View.as_view()
+debugpatterns = []
 
 if settings.DEBUG:
     from django.http import HttpResponseRedirect
     from django.urls import re_path
     
     redirect_to_astro = lambda request: HttpResponseRedirect(f'{settings.ASTRO_URL}{request.path}')
-    urlpatterns += [
+    debugpatterns += [
         re_path(r'^astro', redirect_to_astro),
         re_path(r'\.astro$', redirect_to_astro),
         re_path(r'^src', redirect_to_astro),
         re_path(r'^@id', redirect_to_astro),
         re_path(r'^@vite', redirect_to_astro),
     ]
+
+
+urlpatterns = [
+    path("", include([
+        path("", HomeView.as_view(), name="home"),
+    ], namespace="core")),
+    path("", include(debugpatterns)),
+    path('admin/', admin.site.urls),
+    path('events/<channel>/', events, name='events'),
+    path('api/', include(apipatterns, namespace="api"))
+]
+
+handler404 = Custom404View.as_view()
