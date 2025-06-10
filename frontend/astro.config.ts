79849a015f3feec2e22052d5d16dfdcde5db6b19
Eryk Srebrny <esrebrny@onet.eu>
2025-06-10 20:14:51 +0200

    feat: add env variable defining base url of server to astro config if not in dev mode otherwise to vite config

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -2,57 +2,45 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
 import icon from 'astro-icon';
-import dotenv from "dotenv";
-import node from "@astrojs/node";
+import dotenv from 'dotenv';
+import node from '@astrojs/node';
+
 dotenv.config();
+const isDev = import.meta.env.DEV;
+const baseURL = process.env.ASTRO_BASE_URL ?? "/astro";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
+    base: isDev ? undefined : baseURL,
+    output: 'server',
 
-  vite: {
-    plugins: [tailwindcss()],
-    server: {
-      allowedHosts: [
-        "frontend",
-        "localhost",
-        "host.docker.internal"
-      ]
-    }
-  },
+    vite: {
+      base: isDev ? baseURL : undefined,
+      plugins: [tailwindcss()],
+      server: {
+        allowedHosts: ['frontend', 'localhost', 'host.docker.internal'],
+      },
+    },
 
-  integrations: [
-    alpinejs({ entrypoint: "./src/lib/Alpine/index.ts" }),
-    icon({
-      include: {
-        "ooui": [
-          "article-not-found-ltr", 
-          "upload",
-          "error",
-          "success",
-        ],
-        "mage": ["edit"],
-        "mdi": [
-          "arrow-up-circle",
-          "warning",
-        ],
-        "heroicons": [
-          "x-mark", 
-          "bars-4-16-solid",
-          "information-circle-20-solid",
-        ],
-        "iconoir": ["plus-circle"],
-        "ion": ["*"],
-        "tabler": ["trash"],
-        "tdesign": ["adjustment"],
-        "ep": ["upload-filled"],
-        "fa-solid": ["hands-helping"],
-        "mingcute": ["lock-line"],
-      }
+    integrations: [
+      alpinejs({ entrypoint: './src/lib/Alpine/index.ts' }),
+      icon({
+        include: {
+          ooui: ['article-not-found-ltr', 'upload', 'error', 'success'],
+          mage: ['edit'],
+          mdi: ['arrow-up-circle', 'warning'],
+          heroicons: ['x-mark', 'bars-4-16-solid', 'information-circle-20-solid'],
+          iconoir: ['plus-circle'],
+          ion: ['*'],
+          tabler: ['trash'],
+          tdesign: ['adjustment'],
+          ep: ['upload-filled'],
+          'fa-solid': ['hands-helping'],
+          mingcute: ['lock-line'],
+        },
+      }),
+    ],
+
+    adapter: node({
+      mode: 'standalone',
     }),
-  ],
-
-  adapter: node({
-    mode: "standalone"
-  })
 });
