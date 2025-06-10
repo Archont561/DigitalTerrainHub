7eb76d5453269705cd9618e21f54dd00fdb28ab1
Eryk Srebrny <esrebrny@onet.eu>
2025-06-10 17:05:51 +0200

    feat: move uppy styles to separate uppy.css in styles folder

--- frontend/src/styles/index.ts
+++ frontend/src/styles/index.ts
@@ -1,5 +1,9 @@
-import globalStylesURL from "./global.css?url";
+import Global from "./global.css?url";
+import OpenLayers from "./openlayers.css?url";
+import Uppy from "./uppy.css?url";
 
-export {
-    globalStylesURL,
+export const StylesURLs = {
+    Global,
+    OpenLayers,
+    Uppy,
 }
