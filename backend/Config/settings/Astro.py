53d275d63bbcb907ba29949e23bcfae1b2d71163
Eryk Srebrny <esrebrny@onet.eu>
2025-06-10 19:19:24 +0200

    feat: change implementation of definig ASTRO_URL

--- backend/Config/settings/Astro.py
+++ backend/Config/settings/Astro.py
@@ -1,3 +1,6 @@
 from Config.env import env
 
-ASTRO_URL = env("ASTRO_URL")
+ASTRO_HOST = env("ASTRO_HOST", default="host.docker.internal")
+ASTRO_PORT = env.int("ASTRO_PORT", default=4321)
+ASTRO_URL_BASE = env("ASTRO_URL_BASE", default="")
+ASTRO_URL = f"http://{ASTRO_HOST}:{ASTRO_PORT}/{ASTRO_URL_BASE}"
