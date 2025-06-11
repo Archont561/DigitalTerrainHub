3a2566c38dc4689fd6c31dfcdb3988a63a0c8222
Eryk Srebrny <esrebrny@onet.eu>
2025-06-11 13:48:50 +0200

    fix: add missing icons to config

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -16,45 +16,45 @@ export const isDev = import.meta.env.DEV;
 export const baseURL = process.env.ASTRO_BASE_URL ?? "/astro";
 
 export default defineConfig({
-    base: isDev ? undefined : baseURL,
-    output: 'server',
+  base: isDev ? undefined : baseURL,
+  output: 'server',
 
-    vite: {
-      base: isDev ? baseURL : undefined,
-      plugins: [tailwindcss()],
-      server: {
-        allowedHosts: ['frontend', 'localhost', 'host.docker.internal'],
-      },
+  vite: {
+    base: isDev ? baseURL : undefined,
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['frontend', 'localhost', 'host.docker.internal'],
     },
+  },
 
-    integrations: [
-      alpinejs({ entrypoint: './src/lib/Alpine/index.ts' }), 
-      icon({
-        include: {
-          ooui: ['article-not-found-ltr', 'upload', 'error', 'success'],
-          mage: ['edit'],
-          mdi: ['arrow-up-circle', 'warning'],
-          heroicons: ['x-mark', 'bars-4-16-solid', 'information-circle-20-solid'],
-          iconoir: ['plus-circle'],
-          ion: ['*'],
-          tabler: ['trash'],
-          tdesign: ['adjustment'],
-          ep: ['upload-filled'],
-          'fa-solid': ['hands-helping'],
-          mingcute: ['lock-line'],
-        },
-      }),
-      strictCustomRouting({
-        "/": getView("Home.astro"),
-        "/login": getView("Login.astro"),
-        "/register": getView("Register.astro"),
-        "/profile": getView("Profile.astro"),
-        "components/[...path]": getView("Partials.astro"),
-        "/[...path]": getView("404.astro"),
-      })
-    ],
-
-    adapter: node({
-      mode: 'standalone',
+  integrations: [
+    alpinejs({ entrypoint: './src/lib/Alpine/index.ts' }),
+    icon({
+      include: {
+        ooui: ['article-not-found-ltr', 'upload', 'error', 'success'],
+        mage: ['edit'],
+        mdi: ['arrow-up-circle', 'warning', 'chevron-up', 'bell'],
+        heroicons: ['x-mark', 'bars-4-16-solid', 'information-circle-20-solid', 'bars-4', 'x-mark-16-solid'],
+        iconoir: ['plus-circle'],
+        ion: ['*'],
+        tabler: ['trash'],
+        tdesign: ['adjustment'],
+        ep: ['upload-filled'],
+        'fa-solid': ['hands-helping'],
+        mingcute: ['lock-line'],
+      },
     }),
+    strictCustomRouting({
+      "/": getView("Home.astro"),
+      "/login": getView("Login.astro"),
+      "/register": getView("Register.astro"),
+      "/profile": getView("Profile.astro"),
+      "components/[...path]": getView("Partials.astro"),
+      "/[...path]": getView("404.astro"),
+    })
+  ],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
