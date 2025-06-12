6eb12055c860b40f78d967bb689c665946aba426
Eryk Srebrny <esrebrny@onet.eu>
2025-06-13 01:58:48 +0200

    feat make dynamic rendering of profile page tabs

--- frontend/src/templates/components/tabs/index.ts
+++ frontend/src/templates/components/tabs/index.ts
@@ -1,5 +1,13 @@
+import Home from "./Home.astro";
 import Settings from "./Settings.astro";
+import Workspaces from "./Workspaces.astro";
+import Tasks from "./Tasks.astro";
+import Results from "./Results.astro";
 
 export {
+    Home,
     Settings,
+    Workspaces,
+    Tasks,
+    Results,
 }
