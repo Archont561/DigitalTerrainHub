286bd7b77a835d02bfcd4fc67f3e03cf98e21733
Eryk Srebrny <esrebrny@onet.eu>
2025-06-12 14:27:33 +0200

    feat: change implementation of DaisyUI modal

--- frontend/src/templates/components/utils/index.ts
+++ frontend/src/templates/components/utils/index.ts
@@ -1,12 +1,12 @@
 import Alert from "./Alert.astro";
-import Dialog from "./Dialog.astro";
+import Modal from "./Modal.astro";
 import Dropdown from "./Dropdown.astro";
 import FileDropzone from "./FileDropzone.astro";
 import Popover from "./Popover.astro";
 import DaisyUIThemeSwitcher from "./DaisyUIThemeSwitcher";
 
 export {
-    Dialog,
+    Modal,
     Dropdown,
     FileDropzone,
     Popover,
