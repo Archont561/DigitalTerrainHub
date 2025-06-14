1b846c93762a4cea946034f6132f46455b471d08
Eryk Srebrny <esrebrny@onet.eu>
2025-06-14 22:58:35 +0200

    feat: change subgrid implementation to include all variants

--- frontend/src/templates/layouts/Grid/types.d.ts
+++ frontend/src/templates/layouts/Grid/types.d.ts
@@ -8,10 +8,11 @@ export type ResponsiveProp<T> = T | Partial<Record<Breakpoints, T>>;
 export type GridPlacementMapKeys = keyof typeof GridPlacementMap;
 export type GridContentPlacementMapKeys = keyof typeof GridContentPlacementMap;
 export type NewGridCellPlacement = "row" | "column" | "row dense" | "column dense";
+export type SubgridVariants = "both" | "row" | "col"; 
 
 export interface GridProps {
   inline?: boolean;
-  subgrid?: boolean;
+  subgrid?: SubgridVariants;
   cellsPlacement?: ResponsiveProp<GridPlacementMapKeys>;
   contentPlacement?: ResponsiveProp<GridContentPlacementMapKeys>;
   cols?: ResponsiveProp<GridColRowValue>;
