6b835db70a7489124cda993e3d93426c5f6f31a8
Eryk Srebrny <esrebrny@onet.eu>
2025-06-26 01:25:24 +0200

    feat: use AlpineAjax plugin

--- frontend/src/lib/Alpine/index.ts
+++ frontend/src/lib/Alpine/index.ts
@@ -3,12 +3,18 @@ import GlobalIntervalStore from "./GlobalIntervalStore";
 import persist from "@alpinejs/persist";
 import helpers from "./AlpineHelpersPlugin";
 import semantics from "./AlpineSemanticsPlugin";
+import ajax from "./AlpineAjaxPlugin";
 
 
 export default (Alpine: Alpine) => {
     Alpine.plugin(persist);
     Alpine.plugin(helpers);
     Alpine.plugin(semantics);
+    Alpine.plugin(ajax.setSettings({
+        headers: {
+            "X-CSRF-Token": document.querySelector("meta[name='csrfToken']")?.getAttribute("content") || ""
+        }
+    }));
     Alpine.store("globalInterval", GlobalIntervalStore);
     window.Alpine = Alpine;
 }
