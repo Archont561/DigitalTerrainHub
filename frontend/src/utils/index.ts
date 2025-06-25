b4e8a477cff835b7e7fd911ed1c910a8f24f22a0
Eryk Srebrny <esrebrny@onet.eu>
2025-06-26 01:19:03 +0200

    feat: add html related helper functions to replace element or its content with htmlString

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -2,6 +2,7 @@ import _ from "lodash";
 
 export * from "./time";
 export * from "./url";
+export * from "./html";
 
 export function createCSSVarName(...parts: string[]) {
     return `--${_.filter(parts, Boolean).map(part => _.kebabCase(part)).join("-")}`;
