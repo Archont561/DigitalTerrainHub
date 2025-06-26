4f6d13bbca67a5e21fe9e74119e3717e84b092b0
Eryk Srebrny <esrebrny@onet.eu>
2025-06-26 23:15:47 +0200

    feat: create utility function that enables class instances to be callable with given method

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -11,3 +11,47 @@ export function createCSSVarName(...parts: string[]) {
 export function toTitleCase(str: string) {
     return _.startCase(_.toLower(str));
 }
+
+type CallableInstance<T, A extends any[], R> = T & {
+    (...args: A): R;
+};
+
+export function makeClassCallable<
+    T extends { new (...args: any[]): any },
+    M extends keyof InstanceType<T> = "call"
+>(
+    baseClass: T,
+    classMethodToCall: M = "call" as M
+): new (...args: ConstructorParameters<T>) => CallableInstance<
+    InstanceType<T>,
+    Parameters<InstanceType<T>[M]>,
+    ReturnType<InstanceType<T>[M]>
+> {
+    const handler: ProxyHandler<any> = {
+        get(target, prop, receiver) {
+            return Reflect.get(target, prop, receiver);
+        },
+        apply(target, thisArg, argArray) {
+            const method = target[classMethodToCall];
+            if (typeof method !== "function") {
+                throw new TypeError(`${String(classMethodToCall)} is not a function`);
+            }
+            return method.apply(target, argArray);
+        }
+    };
+
+    return class CallableClass extends baseClass {
+        constructor(...args: any[]) {
+            super(...args);
+
+            // Create callable function delegating to `call()`
+            const callable = (...args: any[]) => {
+                return handler.apply!(this, this, args);
+            };
+
+            Object.setPrototypeOf(callable, this);
+            return new Proxy(callable, handler);
+        }
+    } as any;
+}
+
