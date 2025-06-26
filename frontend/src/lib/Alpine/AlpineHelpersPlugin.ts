7959e1671f9e5a28893b10181384b990be72c06d
Eryk Srebrny <esrebrny@onet.eu>
2025-06-26 11:50:21 +0200

    feat: extend $find magic of alpinejs helpers plugin

--- frontend/src/lib/Alpine/AlpineHelpersPlugin.ts
+++ frontend/src/lib/Alpine/AlpineHelpersPlugin.ts
@@ -3,16 +3,43 @@ import { getRelativeTimeBetweenDates } from "@utils";
 
 declare module "alpinejs" {
     interface Magics<T> {
-        $find: (query: string) => Element | null;
-        $closest: (query: string) => Element | null;
+        $find: FindMagic;
         $relativeTime: typeof getRelativeTimeBetweenDates;
     }
 }
 
+type FindMagic = {
+    <T extends Element = Element>(query: string): T | null;
+    inside: <T extends Element = Element>(query: string) => T | null;
+    closest: <T extends Element = Element>(query: string) => T | null;
+    exists: (query: string) => boolean;
+    count: (query: string) => number;
+    text: (query: string) => string;
+    attr: (query: string, attr: string) => string | null;
+    all: {
+        <T extends Element = Element>(query: string): NodeListOf<T>;
+        inside: <T extends Element = Element>(query: string) => NodeListOf<T>;
+    };
+};
 
 export default function (Alpine: Alpine) {
-    Alpine.magic("find", () => (query: string) => document.querySelector(query));
-    Alpine.magic("closest", el => (query: string) => el.closest(query));
+    Alpine.magic("find", el => {
+        const find = ((query: string) => document.querySelector(query)) as FindMagic;
+
+        find.inside = (query: string) => el.querySelector(query);
+        find.closest = (query: string) => el.closest(query);
+
+        const all = ((query: string) => document.querySelectorAll(query)) as FindMagic["all"];
+        all.inside = (query: string) => el.querySelectorAll(query);
+        find.all = all;
+
+        find.exists = (query: string) => !!find(query);
+        find.count = (query: string) => find.all(query).length;
+        find.text = (query: string) => find(query)?.textContent ?? '';
+        find.attr = (query: string, attr: string) => find(query)?.getAttribute(attr) ?? null;
+
+        return find;
+    });
     Alpine.magic("relativeTime", () => getRelativeTimeBetweenDates);
 
     Alpine.directive("interval", (el, { modifiers, expression }, { evaluate, cleanup }) => {
