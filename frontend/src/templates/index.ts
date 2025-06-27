14054760a8231f738784d3ca88240c645bf87cd7
Eryk Srebrny <esrebrny@onet.eu>
2025-06-28 00:09:16 +0200

    refactor: move pages to separete dir and update routes

--- frontend/src/templates/index.ts
+++ frontend/src/templates/index.ts
@@ -2,14 +2,15 @@ import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const pagesDir = join(__dirname, "pages");
 
 export function getRoutes() {
     return {
-      "/": join(__dirname, "Home.astro"),
-      "/login": join(__dirname, "Login.astro"),
-      "/register": join(__dirname, "Register.astro"),
-      "/profile": join(__dirname, "Profile.astro"),
-      "components/[...path]": join(__dirname, "Partials.astro"),
-      "/[...path]": join(__dirname, "404.astro"),
+      "/": join(pagesDir, "Home.astro"),
+      "/login": join(pagesDir, "Login.astro"),
+      "/register": join(pagesDir, "Register.astro"),
+      "/profile": join(pagesDir, "Profile.astro"),
+      "components/[...path]": join(pagesDir, "Partials.astro"),
+      "/[...path]": join(pagesDir, "404.astro"),
     }
 }
