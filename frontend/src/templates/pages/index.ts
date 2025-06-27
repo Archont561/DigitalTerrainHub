6f70feb77ab251cd63e7ed044d92339f06fb102b
Eryk Srebrny <esrebrny@onet.eu>
2025-06-28 01:18:29 +0200

    feat: in dev mode add additional routes

--- frontend/src/templates/pages/index.ts
+++ frontend/src/templates/pages/index.ts
@@ -4,13 +4,19 @@ import { join, dirname } from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const pagesDir = join(__dirname, "pages");
 
+const prodRoutes = {
+  "/": join(pagesDir, "Home.astro"),
+  "/login": join(pagesDir, "Login.astro"),
+  "/register": join(pagesDir, "Register.astro"),
+  "/profile": join(pagesDir, "Profile.astro"),
+  "/components/[...path]": join(pagesDir, "Partials.astro"),
+  "/[...path]": join(pagesDir, "404.astro"),
+};
+
+const devRoutes = {
+  "/dev": join(pagesDir, "dev", "Dev.astro"),
+};
+
 export function getRoutes() {
-    return {
-      "/": join(pagesDir, "Home.astro"),
-      "/login": join(pagesDir, "Login.astro"),
-      "/register": join(pagesDir, "Register.astro"),
-      "/profile": join(pagesDir, "Profile.astro"),
-      "components/[...path]": join(pagesDir, "Partials.astro"),
-      "/[...path]": join(pagesDir, "404.astro"),
-    }
+  return !import.meta.env.DEV ? prodRoutes : { ...prodRoutes, ...devRoutes };
 }
