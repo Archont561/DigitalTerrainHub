1c7693af3973d3eeb35a1bece90683a1a03cf4b7
Eryk Srebrny <esrebrny@onet.eu>
2025-06-27 18:02:37 +0200

    feat: change makeClassCallable to abstract CallableClass that subclasses Function

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -12,46 +12,13 @@ export function toTitleCase(str: string) {
     return _.startCase(_.toLower(str));
 }
 
-type CallableInstance<T, A extends any[], R> = T & {
-    (...args: A): R;
-};
+type CallableMethods<T> = {
+    [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
+}[keyof T];
 
-export function makeClassCallable<
-    T extends { new (...args: any[]): any },
-    M extends keyof InstanceType<T> = "call"
->(
-    baseClass: T,
-    classMethodToCall: M = "call" as M
-): new (...args: ConstructorParameters<T>) => CallableInstance<
-    InstanceType<T>,
-    Parameters<InstanceType<T>[M]>,
-    ReturnType<InstanceType<T>[M]>
-> {
-    const handler: ProxyHandler<any> = {
-        get(target, prop, receiver) {
-            return Reflect.get(target, prop, receiver);
-        },
-        apply(target, thisArg, argArray) {
-            const method = target[classMethodToCall];
-            if (typeof method !== "function") {
-                throw new TypeError(`${String(classMethodToCall)} is not a function`);
-            }
-            return method.apply(target, argArray);
-        }
-    };
-
-    return class CallableClass extends baseClass {
-        constructor(...args: any[]) {
-            super(...args);
-
-            // Create callable function delegating to `call()`
-            const callable = (...args: any[]) => {
-                return handler.apply!(this, this, args);
-            };
-
-            Object.setPrototypeOf(callable, this);
-            return new Proxy(callable, handler);
-        }
-    } as any;
+export abstract class CallableClass<T> extends Function {
+    constructor(methodName: CallableMethods<T>) {
+        super('...args', `return this.${String(methodName)}(...args)`);
+        return Object.setPrototypeOf(this, new.target.prototype);
+    }
 }
-
