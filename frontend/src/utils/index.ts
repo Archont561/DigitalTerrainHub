ee8b1281ab068290214c0a65c93ac984702fa492
Eryk Srebrny <esrebrny@onet.eu>
2025-06-28 22:55:38 +0200

    refactor: rearange @utils module files

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,41 +1,5 @@
-import _ from "lodash";
-
 export * from "./astro";
 export * from "./time";
 export * from "./url";
 export * from "./html";
-
-export function createCSSVarName(...parts: string[]) {
-    return `--${_.filter(parts, Boolean).map(part => _.kebabCase(part)).join("-")}`;
-}
-
-export function toTitleCase(str: string) {
-    return _.startCase(_.toLower(str));
-}
-
-type CallableMethods<T> = Exclude<{
-    [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
-}[keyof T], keyof Function>;
-
-export abstract class CallableClass<T> extends Function {
-    constructor(methodName: CallableMethods<T>) {
-        super()
-        return new Proxy(this, {
-            apply: (target, thisArg, args) => (target as any)[methodName](...args)
-        });
-    }
-}
-
-
-export function getOrCreateStylesheet(href: string) {
-    let link = document.querySelector(`link[href=${href}]`) as HTMLLinkElement | null;
-    if (link) return link;
-
-    link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = href;
-    link.onload = () => console.log(`Stylesheet loaded: ${href}`);
-    link.onerror = () => console.error(`Failed to load stylesheet: ${href}`);
-    document.appendChild(link);
-    return link;
-}
+export * from "./miscellaneous";
