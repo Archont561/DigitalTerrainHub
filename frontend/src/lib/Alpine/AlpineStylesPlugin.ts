a7f085bba760d065c51ac1b09b732665e50e5dec
Eryk Srebrny <esrebrny@onet.eu>
2025-06-29 06:22:46 +0200

    feat: extend AlpineStylesPlugin by creating CSSPropertyHandler that is exposed via $cssProperty

--- frontend/src/lib/Alpine/AlpineStylesPlugin.ts
+++ frontend/src/lib/Alpine/AlpineStylesPlugin.ts
@@ -1,22 +1,57 @@
 import AlpinePluginBase from "./AlpinePluginBase";
 import type { PluginMagics } from "./AlpinePluginBase";
-import { toggleCSSProperty } from "@utils";
 
 declare module "alpinejs" {
     interface Alpine {
         stylesPlugin: AlpineStylesPlugin;
     }
     interface Magics<T> {
-        $toggleCSSProperty: typeof toggleCSSProperty;
+        $cssProperty: CSSPropertyHandler;
     }
 }
 
+class CSSPropertyHandler {
+    constructor(private el: HTMLElement) {}
+
+    toggle(name: string, value: string) {
+        this.has(name) ? this.remove(name) : this.set(name, value);
+        return this;
+    }
+
+    set(name: string, value: string, important = false) {
+        console.log(name, value)
+        this.el.style.setProperty(name, value, important ? "important" : undefined);
+        return this;
+    }
+
+    isImportant(name: string) {
+        return this.el.style.getPropertyPriority(name) === "important";
+    }
+
+    remove(name: string) {
+        this.el.style.removeProperty(name);
+        return this;
+    }
+
+    get(name: string) {
+        return this.el.style.getPropertyValue(name);
+    }
+
+    has(name: string) {
+        return !!this.get(name);
+    }
+
+    changeElement(el: HTMLElement) {
+        this.el = el;
+        return this;
+    }
+};
 
 class AlpineStylesPlugin extends AlpinePluginBase {
     protected PLUGIN_NAME = "stylesPlugin";
 
     protected magics: PluginMagics = {
-        toggleCSSProperty: () => toggleCSSProperty,
+        cssProperty: (el) => new CSSPropertyHandler(el),
     };
 }
 
