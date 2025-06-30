2fb5a841e24bd3ed7e75660fc6f7b1494c780829
Eryk Srebrny <esrebrny@onet.eu>
2025-06-30 02:09:38 +0200

    feat delete Notifications.ToastContainer and user Utils.ToastContainer in Base Layout

--- frontend/src/templates/components/notifications/index.ts
+++ frontend/src/templates/components/notifications/index.ts
@@ -1,11 +1,9 @@
 import List from "./List.astro"
 import Partial from "./Partial.astro"
 import SSEPartial from "./SSEPartial.astro"
-import ToastContainer from "./ToastContainer.astro"
 
 export {
     List,
     Partial,
     SSEPartial,    
-    ToastContainer,
 }
