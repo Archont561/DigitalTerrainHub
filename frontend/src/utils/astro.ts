77e1f4b5a9c0a94e808461655dcfe45ea4399c17
Eryk Srebrny <esrebrny@onet.eu>
2025-06-30 03:16:39 +0200

    feat: create 2 shortcuts @utils functions for returning HTTP Response

--- frontend/src/utils/astro.ts
+++ frontend/src/utils/astro.ts
@@ -1,5 +1,8 @@
 import _ from "lodash";
 import clsx, { type ClassValue } from "clsx";
+import type { ComponentProps } from "astro/types";
+import { experimental_AstroContainer as AstroContainer, type ContainerRenderOptions } from "astro/container";
+
 
 export function mergeClass(...args: ClassValue[]) {
     return clsx(...args);
@@ -20,3 +23,33 @@ export function mergeStyle(...args: StyleValue[]) {
     return styles.join("");
 }
 
+type AstroComponentFactory = Parameters<AstroContainer["renderToString"]>[0];
+
+type ComponentContainerRenderOptions<T extends AstroComponentFactory> = Omit<ContainerRenderOptions, 'props'> & {
+  // @ts-expect-error
+  props?: ComponentProps<T>;
+};
+
+export async function render<T extends AstroComponentFactory>(
+  Component: T,
+  context: ComponentContainerRenderOptions<T> = {},
+  status = 200,
+): Promise<Response> {
+  const container = await AstroContainer.create();
+  const result = await container.renderToString(Component, context);
+  return new Response(result, {
+    status,
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+    },
+  });
+}
+
+export function jsonify(object: Record<string, unknown> | unknown[], status = 200) {
+  return new Response(JSON.stringify(object), {
+    status,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
