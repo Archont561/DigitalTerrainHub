8481db386119988a535f306f2ec93ecabb7d3eec
Eryk Srebrny <esrebrny@onet.eu>
2025-06-30 04:22:01 +0200

    feat: create exports of @pages module

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,15 @@
+import LoginPage from "./Login.astro";
+import RegisterPage from "./Register.astro";
+import ProfilePage from "./Profile.astro";
+import HomePage from "./Home.astro";
+import NotFoundErrorPage from "./404.astro";
+import ServerErrorPage from "./500.astro";
+
+export {
+    LoginPage,
+    RegisterPage,
+    ProfilePage,
+    HomePage,
+    NotFoundErrorPage,
+    ServerErrorPage,
+}
