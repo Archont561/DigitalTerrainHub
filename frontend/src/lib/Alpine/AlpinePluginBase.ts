6511fe264f96beaecaad81d6e733820915a737d7
Eryk Srebrny <esrebrny@onet.eu>
2025-06-30 10:52:12 +0200

    feat: change implementation of AlpinePluginBase

--- frontend/src/lib/Alpine/AlpinePluginBase.ts
+++ frontend/src/lib/Alpine/AlpinePluginBase.ts
@@ -1,93 +1,117 @@
-import type { 
-    Alpine, 
+import type {
+    Alpine,
     DirectiveData,
     DirectiveUtilities,
     MagicUtilities,
-    ElementWithXAttributes, 
-    Stores, 
+    ElementWithXAttributes,
+    Stores,
     PluginCallback
 } from "alpinejs";
 import { CallableClass } from "@utils";
-import { cloneDeep } from "lodash";
 
-type PluginDirectiveCallback<T extends HTMLElement> = (
-    el: ElementWithXAttributes<T>,
-    directive: DirectiveData,
-    utilities: DirectiveUtilities
-) => void;
-
-interface PluginMagicCallback<T extends HTMLElement> {
-    (el: ElementWithXAttributes<T>, options: MagicUtilities): void;
-};
-
-type StoreCallback<T extends keyof Stores> = (Alpine: Alpine) => Stores[T];
-
-export type PluginDirectives<T extends HTMLElement = HTMLElement> = 
-    Record<string, PluginDirectiveCallback<T>>;
-export type PluginMagics<T extends HTMLElement = HTMLElement> = Record<string, PluginMagicCallback<T>>;
-export type PluginStore = Partial<{
-    [K in keyof Stores]: StoreCallback<K>;
-}>;
-
-interface AlpinePluginSettings { }
-interface AlpinePluginProtocol {
-    install: PluginCallback;
+export declare namespace AlpinePlugin {
+    type DirectiveCallback<T extends HTMLElement = HTMLElement> = (
+        el: ElementWithXAttributes<T>,
+        directive: DirectiveData,
+        utilities: DirectiveUtilities
+    ) => void;
+    
+    interface MagicCallback<T extends HTMLElement = HTMLElement> {
+        (el: ElementWithXAttributes<T>, options: MagicUtilities): void;
+    }
+    
+    type StoreCallback<K extends keyof Stores> = () => Stores[K];
+    
+    interface Settings { }
+    interface Protocol {
+        install: PluginCallback;
+    }
+    
+    interface API<T extends Settings = Settings, E extends HTMLElement = HTMLElement> {
+        addMagic(name: string, callback: MagicCallback<E>): this;
+        addGlobalMagic(name: string, callback: (Alpine: Alpine) => unknown): this;
+        addDirective(name: string, callback: DirectiveCallback<E>, before?: string): this;
+        addStore<K extends keyof Stores>(name: K, callback: StoreCallback<K>): this;
+        addSettings(settings: Partial<T>): this;
+    }
 }
 
+
 export default abstract class AlpinePluginBase<
-    TSettings extends Object = AlpinePluginSettings
-> extends CallableClass<AlpinePluginBase> implements AlpinePluginProtocol {
+    TSettings extends AlpinePlugin.Settings = AlpinePlugin.Settings,
+    Element extends HTMLElement = HTMLElement
+> extends CallableClass<AlpinePluginBase> implements AlpinePlugin.Protocol {
 
     static expose<T extends AlpinePluginBase<any>>(this: new () => T): T & PluginCallback {
         return new this() as T & PluginCallback;
     }
+    
+    public constructor() { super("install") }
 
     protected abstract PLUGIN_NAME: string;
-    protected Alpine!: Alpine;
-    private initialized = false;
+    protected abstract configure(api: AlpinePlugin.API<TSettings, Element>): void;
 
-    constructor() { super("install") }
-
-    protected afterInstall(Alpine: Alpine) { };
-
-    install(Alpine: Alpine) {
+    readonly install = (Alpine: Alpine) => {
         if (this.initialized) {
             console.warn(`Alpine ${this.PLUGIN_NAME} has been already initialized!`);
             return this;
         }
+        this.configure(this.api());
 
-        this.Alpine = Alpine;
         Object.defineProperty(Alpine, this.PLUGIN_NAME, {
-            get() {
-                return this;
-            },
+            get() { return this; },
         });
-        Object.entries(this.directives).forEach(([key, directiveBuilder]) => {
-            Alpine.directive(key, directiveBuilder);
+
+        this.pluginStore.map(([name, callback]) => {
+            Alpine.store(name, callback());
         });
-        Object.entries(this.magics).forEach(([key, callback]) => {
-            Alpine.magic(key, callback);
+        this.directives.map(([name, callback, before]) => {
+            const directive = Alpine.directive(name, callback);
+            before && directive.before(before);
         });
-        Object.entries(this.pluginStore).forEach(([key, callback]) => {
-            callback && Alpine.store(key, callback(Alpine));
+        this.magics.map(([name, callback]) => {
+            Alpine.magic(name, callback);
         });
-        this.afterInstall(Alpine);
+        this.globalMagics.map(([name, callback]) => {
+            Object.defineProperty(Alpine, name, {
+                get() { return callback(Alpine); },
+            });
+        });
+
         this.initialized = true;
         return this;
     }
 
-    protected settings = {} as TSettings;
-    setSettings(settings: Partial<TSettings>) {
-        for (const key in settings) {
-            if (!(key in this.settings)) throw new Error(`Invalid settings property: ${key}`);
-            //@ts-ignore
-            this.settings[key] = settings[key];
-        }
-        return this;
-    };
-    getSettings(): TSettings { return cloneDeep(this.settings) };
+    protected readonly settings = {} as TSettings;
+    private initialized = false;
+    private directives: [string, AlpinePlugin.DirectiveCallback, string | undefined][] = []
+    private magics: [string, AlpinePlugin.MagicCallback][] = [];
+    private globalMagics: [string, (Alpine: Alpine) => unknown][] = [];
+    private pluginStore: [keyof Stores, () => Stores[keyof Stores]][] = [];
 
-    protected directives: PluginDirectives = {};
-    protected magics: PluginMagics = {};
-    protected pluginStore: PluginStore = {};
+    private api() {
+        const self = this;
+        return {
+            addMagic(name, callback: any) {
+                self.magics.push([name, callback]);
+                return this;
+            },
+            addGlobalMagic(name, callback) {
+                self.globalMagics.push([`$${name}`, callback]);
+                return this;
+            },
+            addDirective(name, callback: any, before?) {
+                self.directives.push([name, callback, before]);
+                return this;
+            },
+            addStore(name, callback) {
+                self.pluginStore.push([name, callback]);
+                return this;
+            },
+            addSettings(settings) {
+                Object.assign(self.settings, settings);
+                return this;
+            },
+        } as AlpinePlugin.API<TSettings, Element>;
+    }
 }
